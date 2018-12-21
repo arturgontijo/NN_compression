1d54979f7e60744369dfc7af266eba6898be9e63
arturgontijo <arturgontijo@gmail.com>
2018-12-21 11:23:10 -0200

    First Tests

--- src/data_generation_scripts/generate_sequence_data.py
+++ src/data_generation_scripts/generate_sequence_data.py
@@ -39,7 +39,7 @@ def main():
     flags.n0 = 1.0 - flags.n1
     _keys = ["data_type", "p1", "n1"]
 
-    data = np.empty([flags.num_samples, 1], dtype=[('str', '|S1')])
+    data = np.empty([flags.num_samples, 1], dtype='|S1')
     # print data.shape
     
     if flags.data_type == 'iid':
@@ -72,7 +72,8 @@ def main():
         _keys.append("markovity")
         print("HMM Data generated ...")
 
-    np.savetxt(flags.file_name, data, delimiter='', fmt='%s', newline='')
+    with open(flags.file_name, "wb") as f:
+        np.savetxt(f, data, delimiter='', fmt='%c', newline='')
     
     # print _keys
     args = vars(flags)
